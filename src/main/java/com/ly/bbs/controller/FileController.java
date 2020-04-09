34a34301c6d26e067cd46764ec13c24c13cab65e
q893218728 <614714303@qq.com>
2020-04-09 14:53:42 +0800

    添加Cloud的UFile图片存储。和markdown集成在一起

--- src/main/java/com/ly/bbs/controller/FileController.java
+++ src/main/java/com/ly/bbs/controller/FileController.java
@@ -1,18 +1,36 @@
 package com.ly.bbs.controller;
 
 import com.ly.bbs.entity.File;
+import com.ly.bbs.provider.UcloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 public class FileController {
+    @Autowired
+    UcloudProvider ucloudProvider;
     @PostMapping("/file/upload")
-    public File upload(){
+    public File upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile multipartfile = multipartRequest.getFile("editormd-image-file");
         File file = new File();
-        file.setSuccess(1);
-        file.setMessage("成功");
-        file.setUrl("img/MyChat.png");
+        try {
+            String fileName = ucloudProvider.upload(multipartfile.getInputStream(),multipartfile.getContentType(),multipartfile.getOriginalFilename());
+            file.setSuccess(1);
+            file.setMessage("成功");
+            file.setUrl(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
       return file;
     }
 }
---
+++ src/main/java/com/ly/bbs/provider/UcloudProvider.java
@@ -0,0 +1,71 @@
+package com.ly.bbs.provider;
+
+import cn.ucloud.ufile.UfileClient;
+import cn.ucloud.ufile.api.object.ObjectConfig;
+import cn.ucloud.ufile.auth.ObjectAuthorization;
+import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
+import cn.ucloud.ufile.bean.PutObjectResultBean;
+import cn.ucloud.ufile.exception.UfileClientException;
+import cn.ucloud.ufile.exception.UfileServerException;
+import cn.ucloud.ufile.http.OnProgressListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class UcloudProvider {
+
+    @Value("${ucloud.ufile.public-key}")
+    private String publicKey;
+    @Value("${ucloud.ufile.private-key}")
+    private String privateKey;
+    String buildyoung = "buildyoung";
+
+
+    public String upload(InputStream fileStream,String mimeType,String fileName){
+
+        String generatedFileName;
+        String[] filePaths = fileName.split("\\.");
+        if(filePaths.length>1){
+            generatedFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length-1];
+        }else{
+            return null;
+        }
+
+
+        try {
+            //创建一个授权器
+            ObjectAuthorization objectAuthorization = new UfileObjectLocalAuthorization(publicKey,privateKey);
+            ObjectConfig config = new ObjectConfig("cn-bj","ufileos.com");
+
+            PutObjectResultBean response = UfileClient.object(objectAuthorization,config)
+                    .putObject(fileStream,mimeType)
+                    .nameAs(generatedFileName)
+                    .toBucket(buildyoung)
+                    .setOnProgressListener(new OnProgressListener() {
+                        @Override
+                        public void onProgress(long bytesWritten, long contentLength) {
+
+                        }
+                    })
+                    .execute();
+
+            if(response != null && response.getRetCode() == 0){
+                String url = UfileClient.object(objectAuthorization,config)
+                        .getDownloadUrlFromPrivateBucket(generatedFileName,buildyoung,24*60*60)//一天有效
+                        .createUrl();
+                return url;
+            }
+        } catch (UfileClientException e) {
+            e.printStackTrace();
+        } catch (UfileServerException e) {
+            e.printStackTrace();
+        }
+
+
+
+       return generatedFileName;
+    }
+}
